5437a518c68d8e22f0d547ed1ed009cfad7c83b3
eliza <elizabeth_rubio_24@hotmail.com>
2022-03-01 14:13:01 -0600

    Test

--- src/main/java/entity/Category.java
+++ src/main/java/entity/Category.java
@@ -3,6 +3,7 @@ package entity;
 import javax.persistence.Entity;
 import javax.persistence.Table;
 
+//Suponiendo que hago un camnbio en development
 @Entity
 @Table(name="tbl_categories")
 public class Category {
